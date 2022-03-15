498cbcf154d4e69b6a31d5aae417b2f65f02cab9
shitij <21.shitijagrawal@gmail.com>
2022-03-16 01:24:43 +0530

    Buttons Added

--- main.py
+++ main.py
@@ -19,31 +19,28 @@ def main_menu_keyboard():
 
 
 def start(update: Update, context: CallbackContext):
-	update.message.reply_text("Hello sir,\n Welcome to the COEP FLOSSMEET'22.\n Please Choose the option menu", reply_markup=main_menu_keyboard())
+	update.message.reply_text(
+		text="Hello sir,\n Welcome to the COEP FLOSSMEET'22.\n Please Choose the option menu",
+		reply_markup=main_menu_keyboard())
 
 
 def schedule(update: Update, context: CallbackContext):
-	update.callback_query.message.edit_text("Coming Very Soon.")
+	update.callback_query.message.reply_text(
+		text="Coming Very Soon.",
+		reply_markup=main_menu_keyboard())
 
 
 def join(update: Update, context: CallbackContext):
-	update.callback_query.message.edit_text("Register for FLOSSMEET'22 => https://www.townscript.com/e/flossmeet22-204311")
+	update.callback_query.message.reply_text(
+		text="Register for FLOSSMEET'22 => https://www.townscript.com/e/flossmeet22-204311",
+		reply_markup=main_menu_keyboard())
 
-
-# def help(update: Update, context: CallbackContext):
-# 	update.callback_query.message.edit_text("""Available Commands :-
-# 	/start - To get started
-# 	/join - To join FLOSSMEET'22 on 26th and 27th of March, 2022 🗓
-# 	/floss22 - To get the information about flossMeet22
-# 	/reach - To get the our social media outlet
-# 	/schedule - To get the schedule
-# 	Just ask what is floss ? to know about it...""")
-#
 
 def what_is_flossMeet(update: Update, context: CallbackContext):
 	update.message.reply_photo(
 		photo="https://raw.githubusercontent.com/mr-shitij/TelegramBot_FlossMeet/master/assets/flossmeet.jpeg?token=GHSAT0AAAAAABQYUSJIHLLAR77LVUBQLVXEYRLDG2A",
-		caption="It's all about free and open source software")
+		caption="It's all about free and open source software",
+		reply_markup=main_menu_keyboard())
 
 
 def flossMeet22(update: Update, context: CallbackContext):
@@ -56,13 +53,17 @@ def flossMeet22(update: Update, context: CallbackContext):
 				"you do not miss out on this amazing opportunity. 💻💯There are no branch "
 				"restrictions  or age barriers💫. If you have the desire to learn more and "
 				"contribute to the FLOSS world🐧 make sure "
-				"to register and join us on the 26th and 27th of March, 2022. 🗓")
+				"to register and join us on the 26th and 27th of March, 2022. 🗓",
+		reply_markup=main_menu_keyboard()
+	)
 
 
 def social(update: Update, context: CallbackContext):
-	update.callback_query.message.edit_text(
-		"Linkedin URL => https://www.linkedin.com/company/coep-fossmeet21/, \n"
-		"Website URL => https://foss.coep.org.in/flossmeet/flossmeet22/")
+	update.callback_query.message.reply_text(
+		text="Linkedin URL => https://www.linkedin.com/company/coep-fossmeet21/, \n"
+			 "Website URL => https://foss.coep.org.in/flossmeet/flossmeet22/",
+		reply_markup=main_menu_keyboard()
+	)
 
 
 def unknown(update: Update, context: CallbackContext):
@@ -70,8 +71,14 @@ def unknown(update: Update, context: CallbackContext):
 		what_is_flossMeet(update, context)
 
 	else:
-		update.callback_query.message.edit_text(
-			"Sorry '%s' is not a valid command" % update.message.text)
+		update.callback_query.message.reply_text(
+			text="Sorry '%s' is not a valid command" % update.message.text,
+			reply_markup=main_menu_keyboard()
+		)
+
+
+def error(update, context):
+	print(f'Update {update} caused error {context.error}')
 
 
 def unknown_text(update: Update, context: CallbackContext):
@@ -84,11 +91,11 @@ updater.dispatcher.add_handler(CallbackQueryHandler(social, pattern="reach"))
 updater.dispatcher.add_handler(CallbackQueryHandler(schedule, pattern="schedule"))
 updater.dispatcher.add_handler(CallbackQueryHandler(join, pattern="join"))
 updater.dispatcher.add_handler(CallbackQueryHandler(flossMeet22, pattern="floss22"))
-# updater.dispatcher.add_handler(CallbackQueryHandler(help, pattern="help"))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))  # Filters out unknown commands
 
-# Filters out unknown messages.
+# Filters out unknown messages and error messages.
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+updater.dispatcher.add_error_handler(error)
 
 updater.start_polling()
